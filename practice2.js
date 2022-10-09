e5c8a2cfbefa730269182921517514c4c3210e88
shaikh Azharuddin <112998169+azhar8888@users.noreply.github.com>
2022-10-09 15:51:06 +0530

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,6 @@
+function myFunction()
+        {
+            document.getElementById("demo2").innerHTML="dynamic paragraph created on click"
+            document.getElementById("demo2").stlye.fontSize="larg";
+        
+        };
